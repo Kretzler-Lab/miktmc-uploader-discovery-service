57105057693471c0c12ec332a9feaab7e313d7d7
Nathan Creger <nathancreger1111@gmail.com>
2024-05-08 13:28:35 -0400

    update redirect url

--- app.js
+++ app.js
@@ -285,7 +285,7 @@ function getRedirectUri(homebase) {
         console.warn("KPMP wasn't told where to redirect back to, defaulting.");
         // TODO(cspital) figure out a sane fallback if there is no target
         // target = '&target=/';
-        target = 'https://welcome.kpmp.org/Shibboleth.sso/Login?' + '&SAMLDS=1&target=%2F&';
+        target = 'https://welcome.miktmc.org/Shibboleth.sso/Login?' + '&SAMLDS=1&target=%2F&';
     }
 
     let extra = '';
